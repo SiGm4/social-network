965a599826e388ea3a25a3cf9fb57951cc57c83a
SiGm4 <dimgmx@hotmail.com>
2017-11-02 15:16:41 +0200

    Testing Github Collaboration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    COLLABROS
                 </div>
 
                 <div class="links">
